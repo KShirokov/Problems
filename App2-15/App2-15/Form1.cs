3fe37e827d215b039cb43ba5c784f3cca4bda34e
kshirokov <kshirokov@youdo.com>
2017-08-21 17:29:21 +0300

    решил вторую задачу

---
+++ App2-15/App2-15/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace App2_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ResultMatrix.Text = null;
+
+           var n = TArray.Lines.Length;
+            var m = TArray.Lines[0].Split(',').Length;
+
+            var a = new double[n,m];
+          
+            for (var i = 0; i < n; i++)
+            {
+                var row = TArray.Lines[i].Split(',').Select(val => double.Parse(val.Trim())).ToArray();
+                for (var j = 0; j < m; j++)
+                {
+                    a[i, j] = row[j];
+                }
+            }
+
+            MatrixLineSorter.Sort(a);
+
+            for (var i = 0; i < n; i++)
+            {                
+                for (var j = 0; j < m; j++)
+                {
+                    ResultMatrix.Text = ResultMatrix.Text + a[i, j].ToString("0 ");
+                }
+
+                ResultMatrix.Text = ResultMatrix.Text +  Environment.NewLine;
+            }
+        }
+    }
+}
---
+++ App2-15/App2-15/MatrixLineSorter.cs
@@ -0,0 +1,54 @@
+﻿namespace App2_15
+{
+    public static class MatrixLineSorter
+    {
+
+        private static void SwapRows(double[,] a, int i1, int i2)
+        {
+            var m = a.GetLength(1);
+            //меняем строки исходной матрицы
+            for (var col = 0; col < m; col++)
+            {
+                var val = a[i1, col];
+                a[i1, col] = a[i2, col];
+                a[i2, col] = val;
+            }
+        }
+
+        public static void Sort(double[,] a )
+        {
+            var n = a.GetLength(0);
+            var m = a.GetLength(1);
+
+            var sums = new double[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    sums[i] += a[i, j];
+                }
+            }
+
+            // сортировка вставками
+
+            var i1 = 1;
+            while (i1 < n)
+            {
+                var j1 = i1;
+
+                while (j1 > 0 && sums[j1] > sums[j1 - 1])
+                {
+                    var r = sums[j1];
+                    sums[j1] = sums[j1 - 1];
+                    sums[j1 - 1] = r;
+
+                    SwapRows(a, j1, j1 - 1);
+
+                    j1--;
+                }
+                i1++;
+            }
+        }
+    }
+}
